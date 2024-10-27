0e0e7b1eb071be1ac4eda15b8bc2e237957d13b7
maen <makeupwawo@gmail.com>
2024-10-27 08:17:39 +0400

    products & homepage

--- storefront/src/modules/products/templates/index.tsx
+++ storefront/src/modules/products/templates/index.tsx
@@ -39,10 +39,6 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
         <div className="block w-full relative">
           <ImageGallery images={product?.images || []} />
         </div>
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
-          <ProductInfo product={product} />
-          <ProductTabs product={product} />
-        </div>
         <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-12">
           <ProductOnboardingCta />
           <Suspense

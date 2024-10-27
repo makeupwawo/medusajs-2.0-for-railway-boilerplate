7cbf1d75cc03aebbff4f41e58f989a181a4487e1
maen <makeupwawo@gmail.com>
2024-10-27 08:24:27 +0400

    bg-50

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -3,7 +3,7 @@ import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle m-1">
+    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-blue-50 m-1 ">
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-5 m-1">
         <span>
           <Heading

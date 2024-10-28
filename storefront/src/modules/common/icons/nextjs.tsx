590e0a1dad79b399d5ed6595d5bc5554b11d9a82
maen <makeupwawo@gmail.com>
2024-10-28 04:30:14 +0400

    css +

--- storefront/src/modules/common/icons/nextjs.tsx
+++ storefront/src/modules/common/icons/nextjs.tsx
@@ -10,9 +10,9 @@ const NextJs: React.FC<IconProps> = ({
   return (
     <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="18"
-    height="18"
-    viewBox="0 0 18 18"
+    width="32"
+    height="32"
+    viewBox="0 0 32 32"
     fill="none"
     {...attributes}
   >

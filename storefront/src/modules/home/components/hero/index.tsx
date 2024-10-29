159685e56a23f0b1169ddc2e7a37f8e618f4ec01
maen <makeupwawo@gmail.com>
2024-10-29 10:47:40 +0400

    viewport

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
         <a
           href="/store/"
         >
-          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-2xl shadow">
               Shop Now
           </button>
         </a>

413f4eec5264f5b4445f67e56e5ecef5b9a196a8
maen <makeupwawo@gmail.com>
2024-11-11 20:59:54 +0400

    menu scroll

--- storefront/src/modules/layout/components/side-menu/index.tsx
+++ storefront/src/modules/layout/components/side-menu/index.tsx
@@ -71,6 +71,24 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         )
                       })}
                     </ul>
+                    <div className="scrollHorizantal">
+                    <a href="#home">Home</a>
+                    <a href="#news">News</a>
+                    <a href="#contact">Contact</a>
+                    <a href="#about">About</a>
+                    <a href="#support">Support</a>
+                    <a href="#blog">Blog</a>
+                    <a href="#tools">Tools</a>  
+                    <a href="#base">Base</a>
+                    <a href="#custom">Custom</a>
+                    <a href="#more">More</a>
+                    <a href="#logo">Logo</a>
+                    <a href="#friends">Friends</a>
+                    <a href="#partners">Partners</a>
+                    <a href="#people">People</a>
+                    <a href="#work">Work</a>
+
+                    </div>
                     <div className="flex flex-col gap-y-6">
                       <div
                         className="flex justify-between"
